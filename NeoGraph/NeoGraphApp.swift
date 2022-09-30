59d0516f11b93401671a66e3d3dbec4730106e92
Chon Torres <ironcook67@gmail.com>
2022-09-29 23:30:28 -0700

    Initial Commit

---
+++ NeoGraph/NeoGraphApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeoGraphApp.swift
+//  NeoGraph
+//
+//  Created by Chon Torres on 9/29/22.
+//
+
+import SwiftUI
+
+@main
+struct NeoGraphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
